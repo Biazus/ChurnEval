f5c9da4e9c4343d0c7bf94334be3cbd3734b793c
Miller <miller_br@hotmail.com>
2025-08-01 04:15:53 -0300

    Refactor methods

--- main.py
+++ main.py
@@ -3,7 +3,58 @@ import numpy as np
 from keras.models import Sequential
 from keras.layers import Conv1D, MaxPooling1D, Flatten, Dense
 
-def apply_label_encoding(arr, columns):
+HYPERPARAMS = {
+    "kernel_size": 2,
+    "epochs": 10,
+    "activation": "relu",
+    "optimizer": 'adam',
+    "loss": 'binary_crossentropy',
+    "metrics": ['accuracy']
+}
+
+def build_data_vectors(filename, remove_header=True, remove_first_feature=True):
+    """
+    Loads data from a CSV file and optionally removes the header row and the first feature column.
+    Args:
+        filename (str): Path to the CSV file.
+        remove_header (bool, optional): If True, removes the first row (header). Defaults to True.
+        remove_first_feature (bool, optional): If True, removes the first column (typically an ID). Defaults to True.
+
+    Returns:
+        np.ndarray: The processed data array.
+    """
+    data = np.genfromtxt(filename, delimiter=',', dtype=object)
+    if remove_header:
+        data = data[1:]
+    if remove_first_feature:
+        # usually first column is the ID so we can ignore it
+        data = np.delete(data, 0, axis=1)  # Remove first column (customer id)
+    return data
+
+
+def normalize_data(data, label_column):
+    """
+    Normalizes the input data by applying min-max normalization to numerical columns and label encoding to categorical columns.
+    Separates the label column and returns the processed feature data and labels.
+
+    Args:
+        data (np.ndarray): Input data array.
+        label_column (int): Index of the label column.
+
+    Returns:
+        tuple: A tuple containing the normalized data array (with the label column removed) and the labels array.
+    """
+    for k in range(len(data[0])):
+        try:
+            float(data[0][k])
+            apply_min_max_normalization(data, k)
+        except ValueError:
+            apply_label_encoding(data, k)
+    labels = data[:, label_column]  # Labels is a binary array
+    data = np.delete(data, label_column, axis=1)  # Remove last column (label)
+    return data, labels
+
+def apply_label_encoding(arr, col):
     """
     Encodes categorical columns in a NumPy array using label encoding.
 
@@ -14,18 +65,20 @@ def apply_label_encoding(arr, columns):
     Returns:
         None: The input array is modified in place with normalized values in the specified columns.
     """
-    for i in columns:
-        unique_values = np.unique(arr[:, i])
-        encoder = {label: idx for idx, label in enumerate(unique_values)}
-        arr[:, i] = [encoder[val] for val in arr[:, i]]
+    unique_values = np.unique(arr[:, col])
+    encoder = {label: idx for idx, label in enumerate(unique_values)}
+    arr[:, col] = [encoder[val] for val in arr[:, col]]
+
 
 def safe_convert(value):
+    """Converts the input value to a float, returning np.nan if conversion fails."""
     try:
         return float(value)
     except ValueError:
         return np.nan
 
-def apply_min_max_normalization(arr, columns):
+
+def apply_min_max_normalization(arr, col):
     """
     Applies min-max normalization to specified columns of a NumPy array.
 
@@ -36,20 +89,37 @@ def apply_min_max_normalization(arr, columns):
     Returns:
         None: The input array is modified in place with normalized values in the specified columns.
     """
-    for i in columns:
-        vectorized_conversion = np.vectorize(safe_convert)(arr[:, i])
-        converted_array = np.array(vectorized_conversion, dtype=float)
-        arr[:, i] = converted_array
-        min_val = np.min(arr[:, i].astype(float))
-        max_val = np.nanmax(arr[:, i].astype(float))
-        arr[:, i] = (arr[:, i] - min_val) / (max_val - min_val)
+    vectorized_conversion = np.vectorize(safe_convert)(arr[:, col])
+    converted_array = np.array(vectorized_conversion, dtype=float)
+    arr[:, col] = converted_array
+    min_val = np.min(arr[:, col].astype(float))
+    max_val = np.nanmax(arr[:, col].astype(float))
+    arr[:, col] = (arr[:, col] - min_val) / (max_val - min_val)
 
-def split_dataset(dataset: list,
-            labels: list,
-            num_classes: int,
-            training: float = 0.7,
-            validation: float = 0.15,
-            test: float = 0.15):
+
+def split_dataset(
+        dataset: list,
+        labels: list,
+        training: float = 0.7,
+        validation: float = 0.15,
+        test: float = 0.15
+):
+    """
+    Splits a dataset and corresponding labels into training, validation, and test subsets.
+
+    Args:
+        dataset (list): The input data to be split.
+        labels (list): The labels corresponding to the dataset.
+        training (float, optional): Proportion of data for training. Defaults to 0.7.
+        validation (float, optional): Proportion of data for validation. Defaults to 0.15.
+        test (float, optional): Proportion of data for testing. Defaults to 0.15.
+
+    Returns:
+        dict: Dictionary containing reshaped train, validation, and test sets with their labels.
+
+    Raises:
+        ValueError: If the sum of training, validation, and test proportions is not 1.
+    """
     if sum([training, validation, test]) != 1:
         raise ValueError("Sum of subsets should be 1")
 
@@ -68,31 +138,43 @@ def split_dataset(dataset: list,
         "y_test": labels[idx_test:]
     }
 
-def build_model(dataset: list, num_classes: int,):
+
+def build_model(num_features):
+    """
+    Builds and compiles a 1D convolutional neural network model for binary classification using the specified number of input features.
+
+    Args:
+        num_features (int): Number of features in the input data.
+    Returns:
+        keras.models.Sequential: Compiled Keras model.
+    """
     model = Sequential()
-    model.add(Conv1D(filters=32, kernel_size=2, activation='relu', input_shape=(19, 1)))
+    model.add(Conv1D(
+        filters=32, kernel_size=HYPERPARAMS["kernel_size"], activation=HYPERPARAMS["activation"], input_shape=(num_features, 1))
+    )
     # model.add(MaxPooling1D(pool_size=2))
-
     model.add(Flatten())
-
     model.add(Dense(64, activation='relu'))
-
     model.add(Dense(1, activation='sigmoid'))
-
-    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
-
+    model.compile(
+        optimizer=HYPERPARAMS['optimizer'],
+        loss=HYPERPARAMS['loss'],
+        metrics=HYPERPARAMS['metrics']
+    )
     model.summary()
     return model
 
-def compile_model(m):
-    m.compile(
-        loss='binary_crossentropy',
-        optimizer='adam',
-        metrics=['accuracy']
-    )
-    return m
-
 def train_model(m, vectors):
+    """
+    Trains the given model using provided training and validation data.
+
+    Args:
+        m: A compiled Keras model to be trained.
+        vectors (dict): Dictionary containing 'x_train', 'y_train', 'x_val', and 'y_val' arrays.
+
+    Returns:
+        History object containing training metrics.
+    """
     h = m.fit(
         vectors["x_train"], vectors["y_train"],
         batch_size=16,
@@ -101,28 +183,32 @@ def train_model(m, vectors):
     )
     return h
 
+
 def evaluate_model(m, vectors):
+    """
+    Evaluates the given model on test data and prints the loss and accuracy.
+
+    Args:
+        m: Trained model to be evaluated.
+        vectors (dict): Dictionary containing 'x_test' and 'y_test' data.
+    """
     loss, accuracy = m.evaluate(vectors["x_test"], vectors["y_test"], verbose=0)
     print('Test loss:', loss)
     print('Test accuracy:', accuracy)
 
+
 if __name__ == "__main__":
-    data = np.genfromtxt("WA_Fn-UseC_-Telco-Customer-Churn.csv", delimiter=',', dtype=object)
+    label_column = 19
+    data = build_data_vectors(filename="WA_Fn-UseC_-Telco-Customer-Churn.csv")
+    data, labels = normalize_data(data, label_column)
 
-    # Normalizing Data
-    data = data[1:]  # Remove header
-    data = np.delete(data, 0, axis=1)  # Remove first column (customer id)
-    apply_label_encoding(data, [0, 1, 2, 3, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 19])
-    apply_min_max_normalization(data, [4, 17, 18])
-    labels = data[:, 19]  # Labels is a 0/1 array
-    data = np.delete(data, 19, axis=1)  # Remove last column (label)
+    num_classes = len(set(labels))
+    num_features = data.shape[1]
 
-    v = split_dataset(data, labels, 2)
-
-
-    model = build_model(v["x_train"], 2)
-    model = compile_model(model)
+    v = split_dataset(data, labels)
+    model = build_model(num_features)
     history = train_model(model, v)
+
     print(history.history)
 
     evaluate_model(model, v)
